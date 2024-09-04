6640581e63953895e176a293d92191d26cb1ac05
LucianoSant006 <santiagoluciano383@gmail.com>
2024-09-04 19:08:23 -0300

    lab-24-09-04 heating exrc 01

---
+++ lab_day_2024-09-04/heating_exer01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main()
+{
+    float a,b,c,answer;
+ 
+    printf("digite um número");
+    scanf("%f",&a);
+    printf("digite um número");
+    scanf("%f",&b);
+    printf("digite um número");
+    scanf("%f",&c);
+    
+    
+    answer = abs(a) + abs(b) + abs(c);
+    
+    printf("%.2f",answer);
+    
+    
+    return 0;
+}
